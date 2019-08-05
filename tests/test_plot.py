8d7177233db8dedfe2a17d5b5182e7e151f9e3f9
ikkamens <ikkamens@amazon.com>
2019-08-05 11:24:21 +0200

    test ez_plot

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,5 +1,5 @@
 import os
-from ilya_ezplot import Metric, plot_group
+from ilya_ezplot import Metric, plot_group, ez_plot
 
 
 def test_plot_group(tmpdir):
@@ -17,5 +17,22 @@ def test_plot_group(tmpdir):
     m2.add_record(2, 9)
     m2.add_many(4, [5, 6, -1])
 
-    root_dir = os.path.dirname(__file__)
+    files_before = len(os.listdir(tmpdir))
     plot_group({'m1': m, 'm2': m2}, tmpdir, "bla3")
+    files_after = len(os.listdir(tmpdir))
+
+    assert files_after > files_before
+
+def test_ezplot(tmpdir):
+    m = Metric('x', 'y')
+    m.add_many(1, [1, 2, 3])
+    m.add_record(2, 4)
+    m.add_record(2, 4)
+    m.add_record(2, 4)
+    m.add_many(4, [5, 6, 12])
+
+    files_before = len(os.listdir(tmpdir))
+    ez_plot(m, tmpdir, "bla3")
+    files_after = len(os.listdir(tmpdir))
+
+    assert files_after > files_before
